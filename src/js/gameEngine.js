93eec79d63e1b261abb6ceec7c618a6a3a762a54
Vladislav Peev <vladislavpeev9412@gmai.com>
2022-08-17 12:10:06 +0300

    Added movement boundaries to the wizard

--- src/js/gameEngine.js
+++ src/js/gameEngine.js
@@ -9,17 +9,17 @@ function gameLoop(state,game){
     const {wizard} = state;
     const {wizardElement} = game;
     if(state.keys.KeyD){
-        wizard.posX += wizard.speed;
+        wizard.posX = Math.min(wizard.posX + wizard.speed,game.gameScreen.offsetWidth - wizard.width - 30);
 
     }
     if(state.keys.KeyA){
-        wizard.posX -= wizard.speed;
+        wizard.posX = Math.max(wizard.posX - wizard.speed,6);
     }
     if(state.keys.KeyS){
-        wizard.posY += wizard.speed;
+        wizard.posY = Math.min(wizard.posY + wizard.speed,game.gameScreen.offsetHeight - wizard.height - 30);
     }
     if(state.keys.KeyW){
-        wizard.posY -= wizard.speed;
+        wizard.posY = Math.max(wizard.posY - wizard.speed,6);
     }
     wizardElement.style.left = wizard.posX+"px";
     wizardElement.style.top = wizard.posY+"px";
